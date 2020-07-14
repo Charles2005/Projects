12b633360b2270cec52c8f7297d71ec72398ad85
Charles Abe <abecharles5@gmail.com>
2020-07-14 11:25:53 +0800

    First commit.

---
+++ TextRpg.py
@@ -0,0 +1,18 @@
+class Attributes:
+    def __init__(self):
+        pass
+
+
+class Assassin:
+    def __init__(self):
+        pass
+
+
+class Warrior:
+    def __init__(self):
+        pass
+
+
+class Mage:
+    def __init__(self):
+        pass
